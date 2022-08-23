ee70c8475574ca498dfc536741b2af63e3616a5f
Mikaela-e <tengoos@hotmail.com>
2022-08-23 12:47:24 +0200

    connect to the database try

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require("mongoose")
+const grupparbete = require("../model/test")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+////Bara ett test för att se att det fungerar////
+router.get('/', async(req, res, next) =>{
+  const grupper = await grupparbete.find()
+  res.status(200).json(grupper)
+  
 });
+router.post("/", async(req, res)=>{
+  const grupp = new grupparbete(req.body)
+  await grupp.save();
+  res.status(201).json(grupp)
+})
 
 module.exports = router;
